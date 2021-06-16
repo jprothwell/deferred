4d09fc51144d2bb0983dd97d342b763a7e3f28ef
LeonJing <jprothwell@gmail.com>
2021-06-16 10:01:23 +0800

    A:then,catch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 function Deferred(handler) {
-	if (!(this instanceof Deferred)) {
-		return new Deferred();
-	}
+    if (!(this instanceof Deferred)) {
+        return new Deferred();
+    }
 
-	var self = this;
-	self.promise = new Promise(function(resolve, reject) {
-		if (handler) {
-			handler(resolve, reject);
-		}
-		self.resolve = resolve;
-		self.reject = reject;
-	});
-	return self;
+    var self = this;
+
+    self.promise = new Promise(function(resolve, reject) {
+        if (handler) {
+            handler(resolve, reject);
+        }
+        self.resolve = resolve;
+        self.reject = reject;
+    });
+
+    self.then = self.promise.then.bind(self.promise);
+    self.catch = self.promise.catch.bind(self.promise);
+
+    return self;
 }
 
 export default Deferred;
